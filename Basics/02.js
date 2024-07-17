44eae794e9523877287e89a1d9bd86cf89e8c4b7
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-07-17 19:39:39 +0530

    Data types

---
+++ Basics/02.js
@@ -0,0 +1,29 @@
+// we are learning about datatypes
+
+         /* PRIMITIVE DATA TYPES */
+        // number => 2
+       // bigint  => here we can represent power value
+      // string => ""
+     // boolean => true/false
+    // null =>  0 standalone value
+   // undefined => not defiend
+  // symbol => when we are talking about unicness we use symbol(Basically in react,figma)
+
+
+
+
+let isLogin = true;
+let state = null;
+let age = 19;
+let name = "Reeturaj";
+let bigint = 8986435354578723458783243546534
+
+console.log(name,isLogin,state,bigint)
+
+
+
+
+//OBJECT DATA TYPE 
+//here we are basically finding the dta type of any value
+console.log(typeof null);        //object
+console.log(typeof undefined);   //undefind
