a4e86366d0608f73dfc2277390de9a154157c436
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-07-17 19:39:23 +0530

    Declarartion type

---
+++ Basics/01.js
@@ -0,0 +1,14 @@
+// Type of declaration value in javascript
+
+let name = "Reeturaj";
+const name2 = "Reeturaj";
+var name3 = "Reeturaj";
+console.table([name,name2, name3]);
+
+
+
+/* NOTES */
+
+// but in these days are modern javascript we are not using var because of block scope,function scope.
+// let can be changed but const can't
+//if we are printing the value but did't initialize their value so its print the undefiend
