59812c7bcbee2b39f867b1724c9875dcdb05c4bf
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-07-18 20:04:58 +0530

    basic operations

---
+++ Basics/04.js
@@ -0,0 +1,35 @@
+// we are learning here operations
+let a= 8;
+let negativeValue = -a;
+console.log(negativeValue);
+
+console.log(2+2); // addition
+console.log(2-2);  // substraction
+console.log(2*2);  // multiplication
+console.log(2**2);  // power value
+console.log(2/2); // division
+console.log(2%2); // modulus(reminder left or not)
+
+
+
+// doing concatination methood
+var str1 = "Reetu";
+var str2 = "Raj";
+var str3 = str1+ str2;
+console.log( str3);
+
+
+
+
+//adding string and numbers
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ 2 + 2);
+console.log(1 + 2 + "2");
+
+
+
+
+//doing operation
+console.log(3 + 4 *5 % 3); // wrong way of writing
+console.log((3+5) * 5 % 3); //right way of writing
