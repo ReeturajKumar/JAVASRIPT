479fc57d6678e74faef07768272031b987228eec
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-07-18 20:49:48 +0530

    Summary

---
+++ Basics/dataTypes_Summary.js
@@ -0,0 +1,30 @@
+// Primitive data types :- These are call by value
+// Types of Primitive data types :-  1. String
+//                                   2. Number
+//                                   3. Boolean
+//                                   4. null
+//                                   5. undefend
+//                                   6. Symbol : - used in id for components
+//                                   7. BigInt :- give the value and n in last
+
+
+
+// Non-Premitive type : - These are call by Reference
+// Types of Non-Premitive type : - 1. Array
+//                                 2. Objects
+//                                 3. Function
+
+
+
+// JavaScript is Dynamically typed Language.This means that variables do not have a fixed type .
+
+
+
+//imporrtant note : - function typeof : - Function object
+//                    Array typeof : - Function
+//                    Object typeof : - Function
+
+
+
+
+                                    
