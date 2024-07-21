3a5a8422294f34749dc28b97230160e32e37840a
REETURAJ KUMAR <138992292+ReeturajKumar@users.noreply.github.com>
2024-07-21 13:23:09 +0530

    Object & JSON

---
+++ Basics/14_objectJson.js
@@ -0,0 +1,15 @@
+const course = {
+  courseName : "JAVASCRIPT",
+  courseInstructor : "Reeturaj",
+  CoursePrice : 999,
+}
+
+//firste methood of accessing the value 
+console.log(course.CoursePrice)
+//Second methood of accessing the value
+const {courseInstructor} = course;
+console.log(courseInstructor)
+
+//destructure od seond methood
+const {courseInstructor : namej} = course;
+console.log(namej)
